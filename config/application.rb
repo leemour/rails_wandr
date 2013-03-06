f05c8cc5dcb0fde34237ff30dc46cde0818cc6ab
leemour <leemour@gmail.com>
2013-03-06 22:19:50 +0400

    Finished 1st Model

--- config/application.rb
+++ config/application.rb
@@ -60,5 +60,6 @@ module RailsWandr
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.sass.preferred_syntax = :sass
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 RailsWandr::Application.routes.draw do
+  resources :trips
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
