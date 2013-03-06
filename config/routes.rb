6030590354cb2de5e45be4ed7654bc6e9b5e1843
leemour <leemour@gmail.com>
2013-03-06 22:44:34 +0400

    Added Destination nested in a Trip

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 RailsWandr::Application.routes.draw do
-  resources :trips
+  resources :trips do
+    resources :destinations
+  end
 
+
+  resources :trips
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,7 +55,7 @@ RailsWandr::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'trips#index'
 
   # See how all your routes lay out with "rake routes"
 
