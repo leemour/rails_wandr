4285a6689269af0e1fc17e5ac644f2dcdfd7aa55
leemour <leemour@gmail.com>
2013-03-06 21:44:42 +0400

    Rails travel site initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsWandr::Application.config.secret_token = '9cc5505d16c8ea7ffff0b3adf3bae99a776b53bf3afbe2c75e40393855962eb805d4e3c4b76df3bb0a89fcbd623665ea322f99a552ebc3c39ea03d3cdeb56769'
